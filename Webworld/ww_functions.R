7c51f883a24fbcb73fc160b9cbc60e2ac20199fe
JJ Borrelli <borrejj@gmail.com>
2013-09-10 19:47:26 -0400

    create the project
    added rstudio project and 2 rscripts
    one for the list of functions
    one for the actual simulation script

---
+++ Webworld/ww_functions.R
@@ -0,0 +1,15 @@
+
+#What do I need?
+## Functions
+
+# Function that defines a species by pulling L numbers randomly from 1:K
+
+# Function that then compares the traits between two species to determine the score.Is there:
+#### 1. An interaction 
+#### 2. If there is an interaction:
+######## a. What is the interaction strength
+
+# Function governing population dynamics to be fed into the solver
+
+# Function which determines if there is a speciation event determined by population size
+
---
+++ Webworld/ww_simulation.R
@@ -0,0 +1,2 @@
+# Create K*K matrix
+set.seed(5) #set the seed so that the matrix remains the same
