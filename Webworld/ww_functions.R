88ce1a3562c44063d5b175ca63c0a0bba07f9358
JJ Borrelli <borrejj@gmail.com>
2013-09-12 11:58:22 -0400

    adding functions
    functions for species, score, and an interaction check

--- Webworld/ww_functions.R
+++ Webworld/ww_functions.R
@@ -3,10 +3,29 @@
 ## Functions
 
 # Function that defines a species by pulling L numbers randomly from 1:K
-
+define.species<-function(L,K){        #function takes in number of traits (l) and 
+                                      #the K*K matrix of trait matches
+  traits<-sample(1:K,L)               #pick out the l traits from 1:K
+  return(traits)                      #outputs trait vector
+}
+  
 # Function that then compares the traits between two species to determine the score.Is there:
+trait.score<-function(s1,s2,k.mat){         #input is two trait vectors and the K matrix
+  score.vector<-c()
+  for (i in 1:L){
+    score.vector[i]<-k.mat[s1[i],s2[i]]
+  }
+  score<-sum(score.vector)
+  return(score)
+}
+
 #### 1. An interaction 
+is.interaction<-function(score,threshold){
+  ifelse(score>threshold,return("Interaction"),return("No Interaction"))
+}
+
 #### 2. If there is an interaction:
+
 ######## a. What is the interaction strength
 
 # Function governing population dynamics to be fed into the solver
--- Webworld/ww_simulation.R
+++ Webworld/ww_simulation.R
@@ -1,2 +1,14 @@
+
+# Define K, the total number of traits in the system
+K<-500
+
+# Define L, the number of traits defining a species
+L<-10
+
 # Create K*K matrix
 set.seed(5) #set the seed so that the matrix remains the same
+K.mat<-matrix(nrow=K,ncol=K)
+
+# Set the threshold for scores
+threshold
+
